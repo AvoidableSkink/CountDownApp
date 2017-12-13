5ed6b61089ffa76b59965fe7d424591dbe2cc95d
Kimberly Lindholm <klindholm7@gmail.com>
2017-12-13 16:19:48 -0700

    return mainactivity to its previous glory

---
+++ app/src/main/java/com/example/klind/countdownapp/MainActivity.java
@@ -0,0 +1,124 @@
+package com.example.klind.countdownapp;
+
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteException;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.klind.countdownapp.database.DataSource;
+import com.example.klind.countdownapp.model.Event;
+import com.example.klind.countdownapp.sample.SampleDataProvider;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static com.example.klind.countdownapp.sample.SampleDataProvider.eventItemList;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<Event> eventItemList = SampleDataProvider.eventItemList;
+    DataSource mDataSource;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        mDataSource = new DataSource(this);
+        mDataSource.open();
+        Toast.makeText(this, "Database Created", Toast.LENGTH_SHORT).show();
+
+        long numItems = mDataSource.getEventsCount();
+        if (numItems == 0) {
+            for (Event item : eventItemList) {
+                try {
+                    mDataSource.createItem(item);
+                } catch (SQLiteException e) {
+                    e.printStackTrace();
+                }
+            }
+            Toast.makeText(this, "Data Inserted",
+                    Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this, "Data already present",
+                    Toast.LENGTH_SHORT).show();
+        }
+        Collections.sort(eventItemList, new Comparator<Event>() {
+            @Override
+            public int compare(Event o1, Event o2) {
+                return o1.getEventName().compareTo(o2.getEventName());
+            }
+        });
+
+        EventAdapter adapter = new EventAdapter(this, eventItemList);
+
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean grid = settings.getBoolean(getString(R.string.pref_display_grid), false);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        if (grid) {
+            recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        }
+
+        recyclerView.setAdapter(adapter);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mDataSource.close();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mDataSource.open();
+    }
+}
