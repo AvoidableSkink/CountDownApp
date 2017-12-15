b7613f147309fad6153e06b00d023f8eb6e1cac8
Kimberly Lindholm <klindholm7@gmail.com>
2017-12-14 20:08:21 -0700

    little stufs

--- app/src/main/java/com/example/klind/countdownapp/ChooseImageActivity.java
+++ app/src/main/java/com/example/klind/countdownapp/ChooseImageActivity.java
@@ -1,8 +1,10 @@
 package com.example.klind.countdownapp;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by klind on 12/14/2017.
  */
 
-public class ChooseImageActivity {
+public class ChooseImageActivity extends AppCompatActivity {
 }
